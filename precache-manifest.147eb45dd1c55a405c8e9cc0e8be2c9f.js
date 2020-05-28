64878db015f66fae4540dca73806a64d00f0207f
Alex Kyereboah <atk4kcw@virginia.edu>
2020-05-28 15:49:07 -0400

    Updates

---
+++ precache-manifest.147eb45dd1c55a405c8e9cc0e8be2c9f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dccd3391cd91e8b992f4bacdbb246737",
+    "url": "/kyerebo/school-dashboard/index.html"
+  },
+  {
+    "revision": "aacb7c8ca05986187112",
+    "url": "/kyerebo/school-dashboard/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "b35b8b0e8275ee089f38",
+    "url": "/kyerebo/school-dashboard/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "aacb7c8ca05986187112",
+    "url": "/kyerebo/school-dashboard/static/js/2.a18b5caf.chunk.js"
+  },
+  {
+    "revision": "003c96490c7aaee62db21e3b5f7ebec6",
+    "url": "/kyerebo/school-dashboard/static/js/2.a18b5caf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b35b8b0e8275ee089f38",
+    "url": "/kyerebo/school-dashboard/static/js/main.0771023d.chunk.js"
+  },
+  {
+    "revision": "88faa71f4c9061d593f9",
+    "url": "/kyerebo/school-dashboard/static/js/runtime-main.f4883434.js"
+  }
+]);
